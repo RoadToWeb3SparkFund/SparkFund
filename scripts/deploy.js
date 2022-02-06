d419751ec14c8345d89b00ec6168df8e0e2c476d
rahulnakre <rahulnakre@gmail.com>
2022-02-06 14:37:11 -0500

    seems to deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,17 @@
 // import { Framework } from '@superfluid-finance/sdk-core'
+const SuperfluidSDK = require("@superfluid-finance/js-sdk");
+const { web3tx, toWad } = require("@decentral.ee/web3-helpers");
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const [deployer, owner, fundingRecipient] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // We get the contract to deploy
-  // const CrowdfundFactory = await ethers.getContractFactory('CrowdfundFactory')
-  // const crowdfundFactory = await CrowdfundFactory.deploy()
+  const CrowdfundFactory = await ethers.getContractFactory('CrowdfundFactory')
+  const crowdfundFactory = await CrowdfundFactory.deploy()
 
-  // console.log("CrowdfundFactory deployed to:", crowdfundFactory.address);
+  console.log("CrowdfundFactory deployed to:", crowdfundFactory.address);
 
 
   // const web3Provider = await Moralis.enableWeb3()
@@ -20,52 +22,56 @@ async function main() {
   //   web3Js,
   // })
 
+  const sf = new SuperfluidSDK.Framework({
+    web3,
+    // version: "test",
+    networkName: 'matic',
+    tokens: ["fDAI"]
+  });
+  await sf.initialize();
+
   // const web3ModalSigner = sf.createSigner({ web3Provider: web3Provider })
 
 
   // await sf.initialize()
-  // daix = sf.tokens.fDAIx
-  // dai = await sf.contracts.TestToken.at(await sf.tokens.fDAI.address)
+  daix = sf.tokens.fDAIx
+  dai = await sf.contracts.TestToken.at(await sf.tokens.fDAI.address)
 
   // const factory = await ethers.getContractFactory('CrowdfundFactory')
 
   // const factoryInstance = await factory.deploy()
 
-  // let name = 'dummyCoin'
-  // let symbol = 'DUMB'
-  // let fundingCap = toWad('10000').toString()
-  // let fundingPercent = 20
-  // let tokenScale = (10 ** 16).toString()
-  // let fixedPercent = 20
+  // console.log("facotryInstancedeployed to:", factoryInstance.address);
 
-  // crowdfundContract = await factoryInstance.createCrowdfund(
-  //   name,
-  //   symbol,
-  //   owner,
-  //   fundingRecipient,
-  //   sf.host.address,
-  //   dai.address,
-  //   daix.address,
-  //   fundingCap,
-  //   fundingPercent,
-  //   tokenScale,
-  //   fixedPercent,
-  // )
-}
+  let name = 'dummyCoin'
+  let symbol = 'DUMB'
+  let fundingCap = toWad('10000').toString()
+  let fundingPercent = 20
+  let tokenScale = (10 ** 16).toString()
+  let fixedPercent = 20
 
-
-async function main() {
-  // We get the contract to deploy
-  // const Greeter = await ethers.getContractFactory("Greeter");
-  // const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  // console.log("Greeter deployed to:", greeter.address);
+  crowdfundContract = await crowdfundFactory.createCrowdfund(
+    name,
+    symbol,
+    deployer.address, // owner,
+    deployer.address, // fundingRecipient,
+    sf.host.address,
+    dai.address,
+    daix.address,
+    fundingCap,
+    fundingPercent,
+    tokenScale,
+    fixedPercent,
+  )
 }
 
 
 
 main()
-  .then(() => process.exit(0))
+  .then(() => {
+    console.log("done")
+    process.exit(0)
+  })
   .catch((error) => {
     console.error(error);
     process.exit(1);
